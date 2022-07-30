0a1e31f7eb4e2f8c2ff48842292e04b7f6cba78f
OZY3112 <mrwan311@gmail.com>
2022-07-30 19:22:40 +0200

    package set up

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "assets.example.com",
+      "lh3.googleusercontent.com",
+      "encrypted-tbn0.gstatic.com",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  env: {
+    GOOGLE_OAUTH_ID:
+      "390429814047-ghpuij7loencv92s4qbtrkk4nddlkaoo.apps.googleusercontent.com",
+    NEXT_PUBLIC_SUPABASE_URL: "https://vbdszjtecjwlvwtcskzo.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiZHN6anRlY2p3bHZ3dGNza3pvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTkxOTk3OTQsImV4cCI6MTk3NDc3NTc5NH0.cVvSjVZY8kS9WAlljZ5qmFz8AL-Phj6A1BiLGe-HkCU",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Provider as SupaProvider } from "react-supabase";
+import { ChakraProvider } from "@chakra-ui/react";
+import supabase from "../utils/supa";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.GOOGLE_OAUTH_ID}`}>
+      <SupaProvider value={supabase}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SupaProvider>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
